9fd679fe492088c4443a2ea3a70156cb56ba1e3d
kenjiro_hayashi <kenjiro.forest@gmail.com>
2023-10-12 02:20:34 +0900

    first commit

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+import styles from './NotFound.module.css'
+
+export const NotFound = () => {
+  return (
+    <div className={styles.root}>
+        <p className={styles.text}>hoge</p>
+        <p className={styles.text}>hoge</p>
+        <p className={styles.text}>hoge</p>
+        <p className={styles.text}>hoge</p>
+    </div>
+  )
+}
